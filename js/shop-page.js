f9a99881c4a0c7d7c3f164ab8e4d668ecf11d1b0
lawal <lawalmuktair@gmail.com>
2024-08-10 00:13:24 +0000

    "Filter products to only include clothing and jewelery categories on shop page"

--- js/shop-page.js
+++ js/shop-page.js
@@ -1,5 +1,9 @@
 import { products, renderProducts } from "../data/product.js";
 
+const clothingArray = products.filter((e) => {
+  return e.category.includes("clothing") || e.category.includes("jewelery");
+});
+
 const productContainer2 = document.querySelector(".js-product-container-2");
 
-productContainer2.innerHTML = renderProducts(products);
+productContainer2.innerHTML = renderProducts(clothingArray);
